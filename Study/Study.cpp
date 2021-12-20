df38ed49c59339cfab1e3ecb73aed913cd5f2d08
SeHyunJeon <uhs2222@naver.com>
2021-12-21 01:41:05 +0900

    Title: Study_002

--- Study/Study.cpp
+++ Study/Study.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+	int lucky_number = 3, user_input;
+	std::cout << "Search the my Lucky Number!" << std::endl;
+
+	while (1) {
+		std::cout << "입력: ";
+		std::cin >> user_input;
+		if (user_input == lucky_number) {
+			std::cout << "정답입니다!" << std::endl;
+			return 0;
+		}
+		else {
+			std::cout << "오답입니다!" << std::endl;
+		}
+	}
+
 }
+
