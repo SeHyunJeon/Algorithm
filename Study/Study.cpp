c0ab86c3ad4bb42bbc5ba8b9f17fda24bcf3bd61
SeHyunJeon <uhs2222@naver.com>
2021-12-25 00:47:10 +0900

    study-005

--- Study/Study.cpp
+++ Study/Study.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
 int main() {
-	int a = 5;
-	int* p = &a;
-	cout << "(a)변수 a의 값: " << a << endl;
-	cout << "(&a)변수 a의 주소: " << &a << endl;
-	cout << "(*p)포인터 변수 p의 값: " << *p << endl;
-	cout << "(&p)포인터 변수 p의 주소: " << &p << endl;
-	cout << "(p)그냥 p를 찍었을 때: " << p << endl;
-	
+	int input_test_case = 0;
+
+	cin >> input_test_case;
+
+	for (int i = 0; i < input_test_case; i++) {
+		int score[1000];
+		int input_student = 0;
+		int sum = 0;
+
+		cin >> input_student;
+
+		for (int j = 0; j < input_student; j++) {
+			cin >> score[j];
+			sum += score[j];
+		}
+
+		float avg = 0.0;
+		avg = sum / input_student;
+
+		int result = 0;
+
+		for (int j = 0; j < input_student; j++) {
+			if (score[j] > avg) {
+				result++;
+			}
+		}
+
+		printf("%.3f%%\n", (float)result / (float)input_student * 100);
+	}
 }
