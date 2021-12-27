c121a4fc496f013d6a7cdf388ef8773ff14b1f0f
SeHyunJeon <uhs2222@naver.com>
2021-12-28 03:24:09 +0900

    study-011 (백준 10870번)

--- Study/Study.cpp
+++ Study/Study.cpp
@@ -2,29 +2,22 @@
 
 using namespace std;
 
+int fact(int num);
+
 int main() {
-	int search_room_number = 0;
-	cin >> search_room_number;
+	int input_number = 0;
+	cin >> input_number;
 
-	if (search_room_number == 1) {
-		cout << '1' << endl;
-		return 0;
+	int result = 0;
+	result = fact(input_number);
+
+	cout << result << endl;
+}
+
+int fact(int num) {
+	if (num == 0) {
+		return 1;
 	}
 
-	int floor_max_room_number = 7;
-	int floor = 1;
-
-
-	while (true) {
-		if (floor_max_room_number >= search_room_number) {
-			cout << floor + 1 << endl;
-			break;
-		}
-		else {
-			floor++;
-			floor_max_room_number += 6 * floor;
-		}
-	}
-
-	return 0;
+	return num * fact(num - 1);
 }
