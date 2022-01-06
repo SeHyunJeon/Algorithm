5b7dccfa9355036b0d6741ed4d76c53ecb41a4cc
SeHyunJeon <uhs2222@naver.com>
2022-01-07 03:02:03 +0900

    study-020 (quickSort)

--- Study/Study.cpp
+++ Study/Study.cpp
@@ -1,33 +1,40 @@
 #include <iostream>
+#include <algorithm>
+#include <cstdlib>
 
 using namespace std;
 
+int compare(const void* a, const void* b)    // 오름차순 비교 함수 구현
+{
+	int num1 = *(int*)a;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
+	int num2 = *(int*)b;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
 
+	if (num1 < num2)    // a가 b보다 작을 때는
+		return -1;      // -1 반환
+
+	if (num1 > num2)    // a가 b보다 클 때는
+		return 1;       // 1 반환
+
+	return 0;    // a와 b가 같을 때는 0 반환
+}
 
 int main() {
-	int input_case = 0, i = 0, j = 0, temp = 0;
-
+	int input_case = 0;
 	cin >> input_case;
 
-	int* arr = new int[input_case] {0, };
+	int i = 0;
+	int* arr = new int[input_case];
 
 	for (i = 0; i < input_case; i++) {
 		cin >> arr[i];
 	}
 
-	for (i = 1; i < input_case; i++) {
-		for (j = i; j > 0; j--) {
-			if (arr[j] < arr[j - 1]) {
-				temp = arr[j];
-				arr[j] = arr[j - 1];
-				arr[j - 1] = temp;
-			}
-		}
-	}
+	qsort(arr, input_case, sizeof(int), compare);
 
 	for (i = 0; i < input_case; i++) {
 		cout << arr[i] << endl;
 	}
 
 	delete[] arr;
+	return 0;
 }
