549c02aef9f96d882cb298591e1c907b47dca211
SeHyunJeon <uhs2222@naver.com>
2022-01-08 21:26:41 +0900

    study-021 (백준 2309번, 완전탐색)

--- Study/Study.cpp
+++ Study/Study.cpp
@@ -1,40 +1,54 @@
 #include <iostream>
 #include <algorithm>
-#include <cstdlib>
 
 using namespace std;
 
-int compare(const void* a, const void* b)    // 오름차순 비교 함수 구현
-{
-	int num1 = *(int*)a;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
-	int num2 = *(int*)b;    // void 포인터를 int 포인터로 변환한 뒤 역참조하여 값을 가져옴
-
-	if (num1 < num2)    // a가 b보다 작을 때는
-		return -1;      // -1 반환
-
-	if (num1 > num2)    // a가 b보다 클 때는
-		return 1;       // 1 반환
-
-	return 0;    // a와 b가 같을 때는 0 반환
-}
-
 int main() {
-	int input_case = 0;
-	cin >> input_case;
+	int arr[9] = { 0, };
+	int arr1[7] = { 0, };
 
-	int i = 0;
-	int* arr = new int[input_case];
-
-	for (i = 0; i < input_case; i++) {
+	for (int i = 0; i < 9; i++) {
 		cin >> arr[i];
 	}
 
-	qsort(arr, input_case, sizeof(int), compare);
+	int temp = 0;
 
-	for (i = 0; i < input_case; i++) {
-		cout << arr[i] << endl;
+	for (int i1 = 0; i1 < 9; i1++) {
+		for (int i2 = 0; i2 < 9; i2++) {
+			if (i1 == i2) continue;
+			for (int i3 = 0; i3 < 9; i3++) {
+				if (i1 == i3 || i2 == i3) continue;
+				for (int i4 = 0; i4 < 9; i4++) {
+					if (i1 == i4 || i2 == i4 || i3 == i4) continue;
+					for (int i5 = 0; i5 < 9; i5++) {
+						if (i1 == i5 || i2 == i5 || i3 == i5 || i4 == i5) continue;
+						for (int i6 = 0; i6 < 9; i6++) {
+							if (i1 == i6 || i2 == i6 || i3 == i6 || i4 == i6 || i5 == i6) continue;
+							for (int i7 = 0; i7 < 9; i7++) {
+								if (i1 == i7 || i2 == i7 || i3 == i7 || i4 == i7 || i5 == i7 || i6 == i7) continue;
+								temp = arr[i1] + arr[i2] + arr[i3] + arr[i4] + arr[i5] + arr[i6] + arr[i7];
+								if (temp == 100) {
+									arr1[0] = arr[i1];
+									arr1[1] = arr[i2];
+									arr1[2] = arr[i3];
+									arr1[3] = arr[i4];
+									arr1[4] = arr[i5];
+									arr1[5] = arr[i6];
+									arr1[6] = arr[i7];
+
+									sort(arr1, arr1 + 7);
+
+									for (int i = 0; i < 7; i++) {
+										cout << arr1[i] << endl;
+									}
+
+									return 0;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
-
-	delete[] arr;
-	return 0;
 }
