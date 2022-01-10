3367aefe56cbfa95115d1162d5176438cd3ec9be
SeHyunJeon <uhs2222@naver.com>
2022-01-10 23:45:41 +0900

    study-023

--- Study/Study.cpp
+++ Study/Study.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int arr[10001] = { 0, };
-
 int main()
 {
-    int input_case = 0;
-    cin >> input_case;
+    string input_number = "";
+    cin >> input_number;
+    int size = input_number.length();
 
-    int temp;
-    for (int i = 0; i < input_case; i++) {
-        cin >> temp;
-        arr[temp]++;
+    int* arr = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = input_number[i] - '0';
     }
 
-    for (int i = 1; i <= 10000; i++) {
-        for (int j = 0; j < arr[i]; j++) {
-            cout << i << endl;
-        }
+    sort(arr, arr + size, greater<int>());
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i];
     }
 }
