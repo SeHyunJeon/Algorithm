42c35ca34d48a2da18f79311266c3fb8e35cfb60
SeHyunJeon <uhs2222@naver.com>
2022-01-12 23:18:16 +0900

    study-026(백준 1151번, 좌표정렬)

--- Study/Study.cpp
+++ Study/Study.cpp
@@ -14,13 +14,13 @@ int main() {
 
 	for (int i = 0; i < N; i++) {
 		cin >> x >> y;
-		v.push_back({ x, y });
+		v.push_back({ y, x });
 	}
 
 	sort(v.begin(), v.end());
 
 	for (int i = 0; i < N; i++) {
-		cout << v[i].first << ' ' << v[i].second << '\n';
+		cout << v[i].second << ' ' << v[i].first << '\n';
 	}
 
 	return 0;
