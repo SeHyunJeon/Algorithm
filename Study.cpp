3d1090ea8e9ecc6f868b9bfd1e7110e05f7fbe6e
SeHyunJeon <uhs2921@gmail.com>
2023-07-12 02:08:44 +0900

    study-29

--- Study.cpp
+++ Study.cpp
@@ -4,21 +4,33 @@ using namespace std;
 
 int main()
 {
-    int h, m;
-    int gap;
+    int dice1, dice2, dice3;
 
-    cin >> h >> m;
-    cin >> gap;
+    cin >> dice1 >> dice2 >> dice3;
 
-    if(m + gap >= 60){
-        h += (m + gap) / 60;
-        m = (m + gap) % 60;
+    if (dice1 == dice2 && dice2 == dice3)
+    {
+        cout << 10000 + dice1 * 1000 << endl;
     }
-    else{
-        m += gap;
+    else if (dice1 == dice2 || dice1 == dice3)
+    {
+        cout << 1000 + dice1 * 100 << endl;
     }
-
-    cout << h % 24 << " " << m << endl;
-
-    return 0;
+    else if (dice2 == dice3)
+    {
+        cout << 1000 + dice2 * 100 << endl;
+    }
+    else
+    {
+        int max = dice1;
+        if (max < dice2)
+        {
+            max = dice2;
+        }
+        if (max < dice3)
+        {
+            max = dice3;
+        }
+        cout << max * 100 << endl;
+    }
 }
